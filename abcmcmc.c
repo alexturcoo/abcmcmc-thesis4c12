364c6aac8a275d821fffcb15de5101fa7e5ffc54
opticrom <alexanderturco1@gmail.com>
2022-09-20 16:21:36 -0400

    added number of LCRs in og_protein function

--- abcmcmc.c
+++ abcmcmc.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
+#include <bits/stdc++.h>
+#include <fstream> //The fstream library allows us to work with files
 #include "myRandomFunctions.c"
 
 // THIS IS A TEST TO SEE IF I CAN PUSH AND PULL FROM GITHUB - WORKS
@@ -38,11 +40,24 @@ DSSSSDSSSDSDSSSSSSSSSSDSDSDSDSSSDSDSSGSSDSSSSSDSSSDESTSSDSSDSDSDSDSGSS\
 SELETKEATADESKAEETPASSNESTPSASSSSSANKLNIPAGTDEIKEGQRKHFSRVDRSKINFEAWEL\
 TDNTYKGAAGTWGEKANEKLGRVRGKDFTKNKNKMKRGSYRGGSITLESGSYKFQD";
 
-    int length = srp40.length();
-    int num_lcr = 
-    int avg_entropy_lcr =
+    //creating fasta file of protein string to use in segA
+    std::ofstream MyFile("srp40.fasta"); //Create and open a fasta file
+    MyFile << ">srp40_saccharomyces_cerevisiae\n" << srp40; //Write to the file
+    MyFile.close(); //Close the file
 
-    std::cout << srp40 << "\n" << "\n" << length << "\n";
+    //finding the LENGTH + #ofLCRs
+    int length = srp40.length();
+    system("segA srp40.fasta 15 1.9 2.2 -l | grep '>' | wc -l >> numlcr.txt"); //Write the results of counting the LCRs to a txt file
+    std::ifstream myreadfile("numlcr.txt"); //Declaring fstream variable and connecting it to a stream object by opening the file
+    int num_lcr; //Declare int variable which holds the contents of the 'numlcr.txt' file
+    myreadfile >> num_lcr; //Pipe file's content into stream
+
+    //finding the average entropy of the LCRs
+    //int avg_entropy_lcr =
+
+
+    //TO TEST THE OUTPUT
+    std::cout << srp40 << "\n" << "\n" << length << "\n" << "\n"<< num_lcr << "\n" ;
     return 0;
 
 }
