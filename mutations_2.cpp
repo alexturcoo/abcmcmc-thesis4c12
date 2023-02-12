5d34936c29ac9ff93cb7ad2e0f4a71d83eba6c7b
opticrom <alexanderturco1@gmail.com>
2023-02-12 13:24:46 -0500

    uploaded Brians Mutations update

--- mutations_2.cpp
+++ mutations_2.cpp
@@ -171,3 +171,148 @@ double getNormalDev(double mu, double stdev) {
 //    mutateSeqExp(x); 
 //}
 
+////////// Changed here down - Brian ////////////
+std::string mutateSeqExpBG(std::string simulated_protein, float mutation_rate, float indel_rate, float iterates){
+
+    struct element {
+        char aa;
+        double mut;
+        double ind;
+        int length;
+    };
+    // Setting up the vectors
+    vector<element> protein; 
+    protein.push_back(element()); // create an element
+    char aminoAcids[20] = { 'G', 'A', 'L', 'M', 'F', 'W', 'K', 'Q', 'E',
+        'S', 'P', 'V', 'I', 'C', 'Y', 'H', 'R', 'N', 'D', 'T' };
+                                      
+    std::cout << "before mutateseqEXP:\t" << simulated_protein << "\n"; // Initially printing the non-mutated strin.
+    int len = simulated_protein.length();
+    for (int i = 0; i < simulated_protein.length(); i++) {
+        protein.push_back(element()); // create an element
+        protein[i].aa=simulated_protein[i];
+    }
+
+    // Mutation and indel rate set here now - dont need to set here
+    //float mutation_rate = 0.14;
+    //float indel_rate = 0.14;
+
+    for (int i = 0; i < len; i++) {
+        float beta1 = mutation_rate ; 
+        Expondev myexp(beta1,myran.int64());
+        double deviate = myexp.dev(); // choose exp_deviate(mean of beta)
+        protein[i].mut=deviate;
+    }
+    // Traverse the string
+    for (int i = 0; i < len; i++) {
+        int counter = 1 ;
+        //Code to scan back and forth to find repeats
+        if (protein[i].aa != protein[i+1].aa && protein[i].aa != protein[i-1].aa) {
+            float beta2 = indel_rate ; // the length if no repeats is 1
+            Expondev myexp(beta2,myran.int64());
+            double deviate = myexp.dev();
+            protein[i].ind=deviate ; 
+            protein[i].length=1; 
+        } else {
+            int x = 1 ;
+            int y = 1 ;
+            //Be careful in these while loops, for i-y, when i is 0
+            //and y is 1, how does it not throw error
+            //Looking forward for repeats
+            while (simulated_protein[i] == simulated_protein[i + x]) {
+                counter += 1 ;
+                x++;
+            } 
+            //Looking backwards for repeats
+            while (simulated_protein[i] == simulated_protein[i - y]) {
+                counter += 1 ;
+                y++;
+            }
+            float beta3 = indel_rate * counter ;
+            Expondev myexp(beta3,myran.int64());
+            double deviate = myexp.dev();
+            protein[i].ind=deviate;
+        }
+    }
+
+    for(int iter=0; iter<0.5*len; iter++) { // throw down 0.5 mut/site
+        //selecting the lowest deviate from both vectors
+        double minExpDev=protein[0].mut;
+        int    minPosition=0;
+        int    minType=0; // type=0 for mut, 1 for ins, 2 for del
+        for (int i = 0; i < len; i++) {
+            if(minExpDev > protein[i].mut) {
+                minExpDev=protein[i].mut;
+                minPosition=i; minType=0;
+            }
+            if(minExpDev > protein[i].ind) {
+                minExpDev=protein[i].ind;
+                minPosition=i; minType=1;
+                if(myran.int64() < 0.5) { minType=2; }
+            }
+        }
+        if(minType==0) { // put in a base change
+            protein[minPosition].aa=aminoAcids[myran.int64() % numAA];
+            float beta1 = mutation_rate ; 
+            Expondev myexp(beta1,myran.int64());
+            double deviate = myexp.dev();
+            protein[minPosition].mut=deviate;
+        } else { // put in an indel
+            if(minType==1) { // use insertion
+                if(len < 1.5*len) {
+                    len++;
+                    protein.push_back(element()); // create an element
+                    for(int k=minPosition+1; k<len; k++) {
+                        protein[k]=protein[k-1];
+                    }
+                    float beta1 = mutation_rate ; 
+                    Expondev myexp(beta1,myran.int64());
+                    double deviate = myexp.dev();
+                    protein[minPosition].mut=deviate;
+                }
+            } else { // use deletion
+                if(len > 0.5*len) {
+                    len--;
+                    for(int k=minPosition; k<len-1; k++) {
+                        protein[k]=protein[k+1];
+                    }
+                }
+            }
+        }
+        // check what is around the change
+        // need to look down one, the site and up one
+        for(int l=-1; l<2; l++) { // l=-1,0,1
+            int i=minPosition+l;
+            if (protein[i].aa == protein[i+1].aa || protein[i].aa == protein[i-1].aa) { // part of a repeat
+                int x = i-1;
+                //Look backward
+                while (protein[i].aa == protein[x].aa && x >= 0) { x--; } 
+                int start=x;
+                //count forward
+                x=start; int counter=1;
+                while (protein[start].aa == protein[x].aa && x < len) { 
+                    counter++; x++; 
+                }
+                int end=x;
+                float beta3 = indel_rate * counter ;
+                for(int j=start; j<=end; j++) {
+                    Expondev myexp(beta3,myran.int64());
+                    double deviate = myexp.dev();
+                    protein[j].ind=deviate;
+                    protein[j].length=counter;
+                }
+            } else { // not part of a repeat
+                float beta3 = indel_rate;
+                Expondev myexp(beta3,myran.int64());
+                double deviate = myexp.dev();
+                protein[i].ind=deviate;
+                protein[i].length=1;
+            }
+        }
+    }
+
+    for(int i=0; i<len; i++) { simulated_protein[i]=protein[i].aa; }
+    std::cout << "\n" << "after mutateSeqEXP:\t" << simulated_protein << "\n" << "\n" ;
+    return simulated_protein;
+}
+
